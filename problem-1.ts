6c280531f443f05b26370a877cd76743fe915536
Badhon-Roy <roybadhon286@gmail.com>
2024-11-07 15:04:26 +0600

    problem-1 solved

---
+++ problem-1.ts
@@ -0,0 +1,14 @@
+{
+    // problem 1
+
+    const sumArray = (numbers : number[]) : number =>{
+        return numbers.reduce((acc , current)=> acc + current ,0)
+    }
+
+    console.log(sumArray([1, 2, 3, 4, 5]));
+
+    
+
+    //
+
+}
